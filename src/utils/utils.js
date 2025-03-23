484ef53a6c08562fbe39346f008eed7e250696b2
FelciaPreethi <felciapreethi96@gmail.com>
2025-03-22 18:46:26 -0700

    Add utils function to get formatted date

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,23 @@
+import { differenceInDays, parse, format } from "date-fns";
+
+function formatDateInWords(dateString) {
+  return format(new Date(dateString), "EEE - MMM dd, yyyy");
+}
+
 function formatDate(date) {
+  if (!date) {
+    date = new Date();
+  }
   return date.toISOString().split("T")[0]; // Extracts only the YYYY-MM-DD part
 }
 
-export { formatDate };
+function getDaysBetween(date1, date2) {
+  const format = "yyyy-mm-dd";
+
+  const parsedDate1 = parse(date1, format, new Date());
+  const parsedDate2 = parse(date2, format, new Date());
+
+  return Math.abs(differenceInDays(parsedDate1, parsedDate2));
+}
+
+export { formatDate, formatDateInWords, getDaysBetween };
