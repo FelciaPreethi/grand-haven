260c693c0b816a7556e16f17d53f61ef3639e466
FelciaPreethi <felciapreethi96@gmail.com>
2025-03-22 17:10:21 -0700

    Add links to the navigation in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,37 +2,33 @@ import Bookings from "../Bookings/Bookings";
 import "./Header.scss";
 import { Link } from "react-router-dom";
 
-function Header({ date, setDate, guestCount, setGuestCount }) {
+function Header({ date, setDate, guestCount, setGuestCount, isLoggedIn }) {
   return (
     <header className="header">
-      <img
-        src="/src/assets/logo/hotel-logo.jpg"
-        alt="Logo"
-        className="header__logo"
-      ></img>
+      <Link className="header__link" to="/">
+        <img
+          src="/src/assets/logo/hotel-logo.jpg"
+          alt="Logo"
+          className="header__logo"
+        ></img>
+      </Link>
+
       <nav className="header__nav">
-        <p className="nav__rooms">Rooms</p>
+        <Link
+          className="header__nav-link"
+          to={isLoggedIn ? "/membership" : "/user/login"}
+        >
+          <p className="nav__rooms">Membership</p>
+        </Link>
+
         <Bookings
           date={date}
           setDate={setDate}
           guestCount={guestCount}
           setGuestCount={setGuestCount}
+          buttonName={"Bookings"}
         />
       </nav>
-      {/* <Link to="/login">
-        <div className="login">
-          <svg
-            viewBox="0 0 16 16"
-            xmlns="http://www.w3.org/2000/svg"
-            className="login__icon"
-          >
-            <path
-              d="m 8 1 c -1.65625 0 -3 1.34375 -3 3 s 1.34375 3 3 3 s 3 -1.34375 3 -3 s -1.34375 -3 -3 -3 z m -1.5 7 c -2.492188 0 -4.5 2.007812 -4.5 4.5 v 0.5 c 0 1.109375 0.890625 2 2 2 h 8 c 1.109375 0 2 -0.890625 2 -2 v -0.5 c 0 -2.492188 -2.007812 -4.5 -4.5 -4.5 z m 0 0"
-              fill="#2e3436"
-            />
-          </svg>
-        </div>
-      </Link> */}
     </header>
   );
 }
