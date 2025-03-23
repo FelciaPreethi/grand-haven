93b8a705805fa01711d720d955183be995245c04
FelciaPreethi <felciapreethi96@gmail.com>
2025-03-22 18:45:49 -0700

    Add styling to loginpage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,48 +1,79 @@
 import { useState } from "react";
 import "./LoginPage.scss";
 import "../SignupPage/SignupPage.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function LoginPage() {
+function LoginPage({ setIsLoggedIn }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const backendurl = "http://localhost:8080";
+  const navigate = useNavigate();
 
-  function handleSubmit() {}
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(backendurl + "/user/login", {
+        email: email,
+        password: password,
+      });
+      if (response.status === 200) {
+        console.log(response);
+        localStorage.setItem("jwtToken", response.data.token);
+        localStorage.setItem("Username", response.data.username);
+        localStorage.setItem("email", response.data.email);
+
+        alert("Login successful");
+        setIsLoggedIn(true);
+        navigate("/membership");
+      } else {
+        alert("Please try again");
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Error Logging in. Please try after frew minutes!!");
+    }
+  }
 
   return (
-    <main>
-      <form className="loginform">
-        <input
-          type="email"
-          name="email"
-          placeholder="EMAIL"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-          value={email}
-          className="loginform__input"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="PASSWORD"
-          onChange={(event) => {
-            setPassword(event.target.value);
-          }}
-          value={password}
-          className="loginform__input"
-        />
-        <button
-          type="submit"
-          onSubmit={handleSubmit}
-          className="loginform__button"
-        >
+    <main className="loginpage">
+      <form className="loginform" onSubmit={handleSubmit}>
+        <div class="input-group">
+          <label htmlFor="email" class="input-group__label">
+            EMAIL
+          </label>
+          <input
+            type="email"
+            name="email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            value={email}
+            className="input-group__input"
+          />
+        </div>
+        <div className="input-group">
+          <label className="input-group__label">PASSWORD</label>
+          <input
+            type="password"
+            name="password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            value={password}
+            className="input-group__input"
+          />
+        </div>
+
+        <button type="submit" className="loginform__button">
           LOG IN
         </button>
       </form>
-      <div>
+
+      <div className="signup">
         <p>Don't have an account yet.</p>
-        <Link to="/signup">
+        <Link to="/user/signup">
           <p>SignUp</p>
         </Link>
       </div>
