16869519d6b2d48eaf45091d2df61b0b7e8291d1
FelciaPreethi <felciapreethi96@gmail.com>
2025-10-16 20:36:42 -0700

    Add backend url for admin login

--- src/pages/AdminLogin/AdminLogin.jsx
+++ src/pages/AdminLogin/AdminLogin.jsx
@@ -8,7 +8,7 @@ import axios from "axios";
 function AdminLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const backendurl = "http://:8080";
+  const backendurl = import.meta.env.VITE_BACKEND_URL;
   const navigate = useNavigate();
 
   async function handleSubmit(event) {
